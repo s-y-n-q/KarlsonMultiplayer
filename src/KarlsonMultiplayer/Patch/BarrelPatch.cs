f6b22f56ef46f1634b989dfa09911914cc107ef7
synq <nothingdudeasdasd@gmail.com>
2024-09-17 17:49:35 -0700

    Add files via upload

---
+++ src/KarlsonMultiplayer/Patch/BarrelPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using KarlsonMultiplayer.Utils;
+using KarlsonMultiplayer;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[HarmonyPatch(typeof(Barrel))]
+internal class BarrelPatch
+{
+    [HarmonyPatch("Explode")]
+    [HarmonyPostfix]
+    private static void PostExplode()
+    {
+        Console.WriteLine("barrel explosion");
+    }
+}
---
+++ src/KarlsonMultiplayer/Patch/WeaponPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using KarlsonMultiplayer;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[HarmonyPatch(typeof(RangedWeapon))]
+internal class WeaponPatch
+{
+    [HarmonyPatch("SpawnProjectile")]
+    [HarmonyPostfix]
+    private static void PostShoot(Vector3 attackDirection)
+    {
+        foreach (Friend friend in Load.CurrentLobby.Members)
+        {
+            if (!friend.IsMe)
+            {
+                SteamNetworking.SendP2PPacket(friend.Id, Encoding.UTF8.GetBytes($"shootgun|{attackDirection.x}|{attackDirection.y}|{attackDirection.z}"));
+            }
+        }
+    }
+}
+
